49afe5d06a15e6eb363d85be374d7a3a341309ef
danish siddiqui <danishsiddiqui6789@gmail.com>
2023-02-23 04:05:30 +0500

    deployment link removed for axios request

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import Routes from "./config/routes";
 import Header from "./components/header";
-import axios from "axios";
-
-axios.defaults.baseURL = "https://reception-app-mern.vercel.app";
 
 function App() {
   return (
