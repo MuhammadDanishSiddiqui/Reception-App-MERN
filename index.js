0e4102c3271b6fb5be44709415f377e2faeacf31
danish siddiqui <danishsiddiqui6789@gmail.com>
2023-02-23 04:18:42 +0500

    deploy issue

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const visitorsRouter = require("./routes/visitors.route");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
 app.use(express.json());
 app.use("/api", visitorsRouter);
 
