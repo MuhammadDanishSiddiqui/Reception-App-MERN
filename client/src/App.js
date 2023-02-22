ffec19653ec3a81475ac60e08c4f622f4dd6c869
danish siddiqui <danishsiddiqui6789@gmail.com>
2023-02-23 04:59:46 +0500

    deploy issues

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import Routes from "./config/routes";
 import Header from "./components/header";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://reception-app-mern.vercel.app";
 
 function App() {
   return (
