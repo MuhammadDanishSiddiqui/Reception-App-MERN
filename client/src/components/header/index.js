ef09432aa24b827078b72b0c457ac6c7160d9243
danish siddiqui <danishsiddiqui6789@gmail.com>
2023-02-23 06:28:29 +0500

    fixed header

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="flex justify-between px-8 py-4 bg-blue-600">
+    <div className="flex flex-col sm:flex-row items-center justify-between px-8 py-4 bg-blue-600">
       <div>
         <h1 className="text-white font-bold text-xl">Receptionist App</h1>
       </div>
-      <div>
+      <div className="mt-2 sm:mt-0">
         <Link className="text-white text-lg mr-8" to={"/entry"}>
           Register
         </Link>
--- client/src/components/visitors/index.js
+++ client/src/components/visitors/index.js
@@ -30,7 +30,7 @@ const Visitors = () => {
 
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-8 px-8">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <table className="w-full table-auto text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" className="px-6 py-3">
